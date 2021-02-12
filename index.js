ca773dce4bcc359a429038b7e421b7b2f78fad8d
Trayshawn <traytwebb14@yahoo.com>
2021-02-12 02:01:28 -0600

    commit tree e18b0dfebe34a8e5f9dc06ed76df04a545c2311b

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let input = document.querySelector('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+      });
 }
